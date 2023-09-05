427a7a5668f7b2e3518dcead050953bbf9f1548e
MarceloOrtiz <marceloortizz@gmail.com>
2023-09-05 10:11:37 -0300

    ej2sql

--- Clase 01/Marcelo01.sql
+++ Clase 01/Marcelo01.sql
@@ -15,41 +15,48 @@ BEGIN
 END $$
 DELIMITER ;
 
-SELECT
-    DATE(OrderDate),
-    COUNT(`OrderDate`) as cant_ord
+-- 10 Fechas con mayores ordenes
+SELECT DATE(OrderDate), COUNT(`OrderDate`) as cant_ord
 FROM salesorderheader
 GROUP BY DATE(OrderDate)
-ORDER BY cant_ord DESC;
+ORDER BY cant_ord DESC
+LIMIT 10;
+
+-- 10 Fechas con menores ordenes
+SELECT DATE(OrderDate), COUNT(`OrderDate`) as cant_ord
+FROM salesorderheader
+GROUP BY DATE(OrderDate)
+ORDER BY cant_ord
+LIMIT 10;
 
 CALL CantidadDeOrdenes('2004-05-01');
-
 CALL CantidadDeOrdenes('2004-03-01');
-
 CALL CantidadDeOrdenes('2004-02-01');
-
 CALL CantidadDeOrdenes('2020-02-01');
 
 -- 2. Crear una función que calcule el valor nominal de un margen bruto
-
 -- determinado por el usuario a partir del precio de lista de los productos.
+SET GLOBAL log_bin_trust_function_creators=1;
+
 
 DROP FUNCTION margenBruto;
 
 DELIMITER $$
-
-CREATE PROCEDURE CANTIDADDEORDENES(IN FECHA DATE) BEGIN 
-SELECT 
-	SELECT
-	SELECT
-	SELECT
-	SELECT
-	SELECT
-	SELECT
-	    COUNT(OrderDate) as Cantidad_Ordenes
-	FROM salesorderheader
-	WHERE DATE(OrderDate) = fecha;
-	END $$ 
-
-
+CREATE FUNCTION margenBruto(
+	precio DECIMAL(10,3),
+	margen DECIMAL(10,2))  -- valor porcentual Ej: 22% 
+	RETURNS DECIMAL(10,3)
+	DETERMINISTIC
+BEGIN 
+	DECLARE margenBruto DECIMAL(10,3);
+	SET margenBruto = precio + precio*margen/100;
+	RETURN margenBruto;
+END $$ 
 DELIMITER ;
+
+SELECT margenBruto(100, 20) as Precio_Final;
+SELECT margenBruto(100, -25) as Precio_Final;
+SELECT margenBruto(120.50, 31.17) as Precio_Final;
+
+
+

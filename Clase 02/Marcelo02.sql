85118475501513f1bf7c2842c91014bf87ed0f1b
MarceloOrtiz <marceloortizz@gmail.com>
2023-09-07 11:45:12 -0300

    ej1

---
+++ Clase 02/Marcelo02.sql
@@ -0,0 +1,35 @@
+USE adventureworks;
+
+
+--1)  Obtener un listado contactos que hayan ordenado productos
+-- de la subcategoría "Mountain Bikes", entre los años 2000 y 2003,
+-- cuyo método de envío sea "CARGO TRANSPORT 5".
+
+
+-- buscar las tablas a trabajar
+SELECT * FROM product;
+DESCRIBE product;
+
+SELECT * from ProductSubcategory;
+DESCRIBE ProductSubcategory;
+
+SELECT * from ProductCategory;
+DESCRIBE ProductCategory;
+
+SELECT * FROM salesorderdetail;
+DESCRIBE salesorderdetail;
+
+SELECT * FROM salesorderheader;
+DESCRIBE salesorderheader;
+
+SELECT * from Contact;
+DESCRIBE Contact;
+
+
+
+
+
+SELECT * FROM contact;
+
+DESCRIBE contact;
+SHOW COLUMNS FROM contact;

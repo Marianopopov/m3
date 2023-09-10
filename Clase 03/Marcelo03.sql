bc192dc776c721c50c1139c93af4d2617cfb0d84
MarceloOrtiz <marceloortizz@gmail.com>
2023-09-10 15:48:21 -0300

    ej2 subconsulta en select

--- Clase 03/Marcelo03.sql
+++ Clase 03/Marcelo03.sql
@@ -1,6 +1,13 @@
 USE adventureworks;
 
-SELECT YEAR(`OrderDate`), s.`Name`, SUM(`OrderQty`)
+-- 1. Obtener un listado de cuál fue el volumen de ventas (cantidad) por año 
+-- y método de envío mostrando para cada registro, qué porcentaje representa 
+-- del total del año. Resolver utilizando Subconsultas y Funciones Ventana, 
+-- luego comparar la diferencia en la demora de las consultas.
+
+SELECT YEAR(`OrderDate`)
+        , s.`Name`
+        , SUM(`OrderQty`)
     FROM salesorderheader h
     JOIN shipmethod s
         ON h.`ShipMethodID`=s.`ShipMethodID`
@@ -48,3 +55,43 @@ SELECT anio, metodo, suma cantidad, ROUND(100*suma/SUM(suma) OVER (PARTITION BY 
                 ON h.`SalesOrderID`=d.`SalesOrderID`
             GROUP BY 1,2) as sub
 
+
+
+-- 2. Obtener un listado por categoría de productos,
+-- con el valor total de ventas y productos vendidos, 
+-- mostrando para ambos, su porcentaje respecto del total.
+
+SELECT sum(LineTotal)
+    FROM salesorderdetail
+
+
+SELECT sum(`OrderQty`)
+    FROM salesorderdetail
+
+SELECT c.`Name`, SUM(`LineTotal`), SUM(`OrderQty`)
+    FROM salesorderdetail d
+    JOIN product p
+        ON d.`ProductID`=p.`ProductID`
+    JOIN productsubcategory s
+        ON p.`ProductSubcategoryID`=s.`ProductSubcategoryID`
+    JOIN productcategory c
+        ON s.`ProductCategoryID`=c.`ProductCategoryID`
+GROUP BY 1
+
+
+-- RESOLUCION CON SUBCONSULTA EN EL SELECT
+SELECT c.`Name`
+    , ROUND(SUM(`LineTotal`),2) valor_total
+    , ROUND(100*SUM(`LineTotal`)/(
+        SELECT sum(LineTotal) FROM salesorderdetail),2) "%_valor_total"
+    , ROUND(SUM(`OrderQty`),2) prod_vendidos
+    , ROUND(100*SUM(`OrderQty`)/(SELECT sum(`OrderQty`) FROM salesorderdetail),2) "%_prod_vendidos"
+    FROM salesorderdetail d
+    JOIN product p
+        ON d.`ProductID`=p.`ProductID`
+    JOIN productsubcategory s
+        ON p.`ProductSubcategoryID`=s.`ProductSubcategoryID`
+    JOIN productcategory c
+        ON s.`ProductCategoryID`=c.`ProductCategoryID`
+GROUP BY 1
+ORDER BY 1
